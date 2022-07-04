d54ac864a3ca9342de0bdc6fa8c3d9a8826afb5b
dothanhtinh <dothanhtinh2ne1@gmail.com>
2022-07-04 17:41:22 +0700

    Edit: Route home page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home-page', function () {
+    return view('home_page');
 });
