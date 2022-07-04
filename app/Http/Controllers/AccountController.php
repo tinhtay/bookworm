d403de739391b9a5961a2b609535ae83fe9ef478
dothanhtinh <dothanhtinh2ne1@gmail.com>
2022-07-04 17:35:07 +0700

    Edit: login, logout, Add: Register

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,10 +4,80 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\AccountRepositories;
+use Illuminate\Support\Facades\Auth ;
+use Illuminate\Support\Facades\hash ;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class AccountController extends Controller
 {
-    /**
+   
+    public function __construct()
+    {
+        $this->query = User::query();
+    }
+    public function register(Request $request)
+    {
+
+        $request->validate([
+            'first_name'    =>'required|string',
+            'last_name'     =>'required|string',
+            'email'         =>'required|string',
+            'password'      =>'required|string',      
+        ]);
+        $user = new User([
+            'first_name'    => $request -> first_name ,
+            'last_name'     => $request -> last_name ,
+            'email'         => $request -> email,
+            'password'      => Hash::make($request ->password)
+        ]);
+        $user->save();
+        return response()->json(['message'=>'Success registered'],200);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        if(!Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            return response()->json(['message' => 'Invalid login details'], 401);
+        }
+
+        $user  = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user'          => Auth::user(),
+            'access_token' => $token,
+            'token_type'   => 'Bearer',
+        ]);
+    }
+
+
+
+    public function logout(User $user){
+
+        $user->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+    public function me()
+    {
+        return Auth::guard('sanctum')->user();
+    }
+
+
+     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -61,4 +131,5 @@ class AccountController extends Controller
     {
         //
     }
+
 }
