3b477c0f464d557f672397c6b99ced7901f487c2
dothanhtinh <dothanhtinh2ne1@gmail.com>
2022-07-04 17:40:13 +0700

    Edit render for App.js

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Welcome from './welcome';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../app';
+
 
 ReactDOM.render(
-  <Welcome/>,
+
+     <React.StrictMode>
+      <Router>
+        <App/>
+      </Router>
+    </React.StrictMode> ,
   document.getElementById('root')
 );
