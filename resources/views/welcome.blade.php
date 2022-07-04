3d1628d161d6e0de69273f176ceb9f5ed982494d
dothanhtinh <dothanhtinh2ne1@gmail.com>
2022-07-04 17:40:41 +0700

    New: create view home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Welcome to Book Worm</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body class="antialiased">
         <div id="root"></div>
         <script src="{{mix('/js/app.js')}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
