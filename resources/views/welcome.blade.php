d37daa126ad729be17fc4340d971a021f6944daa
dothanhtinh <dothanhtinh2ne1@gmail.com>
2022-07-05 17:22:27 +0700

    edit: title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Welcome to Book Worm</title>
+        <title>Book Worm</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body class="antialiased">
